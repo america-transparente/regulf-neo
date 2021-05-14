1bfc708d286f094a100418e0ab6c5b0efe7c68a5
Benjamín Vicente <benjavicente@uc.cl>
2021-05-14 19:31:48 -0400

    feat: date range component

--- src/app.js
+++ src/app.js
@@ -33,19 +33,43 @@ search.addWidgets([
   }),
   instantsearch.widgets.infiniteHits({
     container: '#hits',
+    transformItems(items) {
+      return items.map(function (item) {
+        return {
+          ...item,
+          fecha_ingreso: item.fecha_ingreso.replaceAll("/", "-"),
+          fecha_término: item.fecha_término.replaceAll("/", "-"),
+        }
+      })
+    },
+    cssClasses: {
+      list: "query-results",
+      item: "query-result-item"
+    },
     templates: {
       item: `
-      <div class="hit-name">
-        <p>{{#helpers.snippet}}{ "attribute": "nombre", "highlightedTagName": "mark" }{{/helpers.snippet}}</p>
-      </div>
-      <div class="hit-data">
-        <div class="hit-key">"fecha_publicación":</div> {{fecha_publicación}}<br>
-        <div class="hit-key">"tipo_cargo":</div> {{#helpers.snippet}}{ "attribute": "tipo_cargo", "highlightedTagName": "mark" }{{/helpers.snippet}}<br>
-        <div class="hit-key">"nombre_organismo":</div> {{nombre_organismo}}<br>
-        <div class="hit-key">"tipo_calificación_profesional":</div> {{tipo_calificación_profesional}}<br>
-        <div class="hit-key">"tipo_contrato":</div> {{tipo_contrato}}
-      </div>
-      `,
+        <p class="hit-name">
+          {{#helpers.snippet}}{ "attribute": "nombre", "highlightedTagName": "mark" }{{/helpers.snippet}}
+        </p>
+
+        <div class="hit-time-range">
+          Desde <time datetime="{{hit.fecha_ingreso}}" title="fecha ingreso">{{fecha_ingreso}}</time>
+          {{#fecha_término}}
+            hasta <time datetime="{{fecha_término}}" title="fecha término">{{fecha_término}}</time>
+          {{/fecha_término}}
+        </div>
+
+        <div class="hit-data">
+          <details>
+            <div class="hit-key">"fecha_publicación":</div> {{fecha_publicación}}<br>
+            <div class="hit-key">"tipo_cargo":</div> {{#helpers.snippet}}{ "attribute": "tipo_cargo", "highlightedTagName": "mark" }{{/helpers.snippet}}<br>
+            <div class="hit-key">"nombre_organismo":</div> {{nombre_organismo}}<br>
+            <div class="hit-key">"tipo_calificación_profesional":</div> {{tipo_calificación_profesional}}<br>
+            <div class="hit-key">"tipo_contrato":</div> {{tipo_contrato}}
+          </details>
+        </div>
+        <button class="hot-show-more">Más información</button>
+        `.replaceAll(/\n\s+/g, '\n'),
       showMoreText: 'Mostrar más',
     },
   }),
