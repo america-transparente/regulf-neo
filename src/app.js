39b4805d29e7e5f911557882c83d7882441632f5
Benjamín Vicente <benjavicente@uc.cl>
2021-05-15 00:02:02 -0400

    feat: new custom style and layout

--- src/app.js
+++ src/app.js
@@ -34,17 +34,15 @@ search.addWidgets([
   instantsearch.widgets.infiniteHits({
     container: '#hits',
     transformItems(items) {
-      return items.map(function (item) {
-        return {
-          ...item,
-          fecha_ingreso: item.fecha_ingreso.replaceAll("/", "-"),
-          fecha_término: item.fecha_término.replaceAll("/", "-"),
-        }
-      })
+      return items.map(item => ({
+        ...item,
+        fecha_ingreso: item.fecha_ingreso.replaceAll('/', '-'),
+        fecha_término: item.fecha_término.replaceAll('/', '-'),
+      }));
     },
     cssClasses: {
-      list: "query-results",
-      item: "query-result-item"
+      list: 'query-results',
+      item: 'query-result-item',
     },
     templates: {
       item: `
@@ -52,23 +50,26 @@ search.addWidgets([
           {{#helpers.snippet}}{ "attribute": "nombre", "highlightedTagName": "mark" }{{/helpers.snippet}}
         </p>
 
-        <div class="hit-time-range">
-          Desde <time datetime="{{hit.fecha_ingreso}}" title="fecha ingreso">{{fecha_ingreso}}</time>
+        <div class="hit-contract">
+          <span>Tipo: <span title="tipo del contrato">{{tipo_contrato}}</span></span>
+          <span>Renumeración:  <span title="renumeración líquida">{{remuneración_líquida_mensual}}</span></span>
+        </div>
+
+        <div class="hit-info">
+          Trabajando como
+          <span class="hit-body-key" title="tipo cargo">{{tipo_cargo}}</span>
+          en
+          <span class="hit-body-key" title="nombre organismo">{{nombre_organismo}}</span>
+          desde
+          <time class="hit-body-key" datetime="{{hit.fecha_ingreso}}" title="fecha ingreso">{{fecha_ingreso}}</time>
           {{#fecha_término}}
-            hasta <time datetime="{{fecha_término}}" title="fecha término">{{fecha_término}}</time>
+            hasta <time class="hit-body-key" datetime="{{fecha_término}}" title="fecha término">{{fecha_término}}</time>
           {{/fecha_término}}
         </div>
 
-        <div class="hit-data">
-          <details>
-            <div class="hit-key">"fecha_publicación":</div> {{fecha_publicación}}<br>
-            <div class="hit-key">"tipo_cargo":</div> {{#helpers.snippet}}{ "attribute": "tipo_cargo", "highlightedTagName": "mark" }{{/helpers.snippet}}<br>
-            <div class="hit-key">"nombre_organismo":</div> {{nombre_organismo}}<br>
-            <div class="hit-key">"tipo_calificación_profesional":</div> {{tipo_calificación_profesional}}<br>
-            <div class="hit-key">"tipo_contrato":</div> {{tipo_contrato}}
-          </details>
+        <div class="hit-button-container">
+          <button class="hit-show-more">Más información</button>
         </div>
-        <button class="hot-show-more">Más información</button>
         `.replaceAll(/\n\s+/g, '\n'),
       showMoreText: 'Mostrar más',
     },
